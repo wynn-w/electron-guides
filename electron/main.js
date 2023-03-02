dfe04ff6de85d5573e6461d3523bb443e658577b
wynn-w <wwwynn22@gmail.com>
2023-03-03 01:09:33 +0800

    electron init

---
+++ electron/main.js
@@ -0,0 +1,29 @@
+
+const { app, BrowserWindow, screen,globalShortcut }= require('electron');
+let mainWindow= null;
+class createWin {
+  constructor() {
+    const displayWorkAreaSize = screen.getAllDisplays()[0].workArea;
+    mainWindow = new BrowserWindow({
+      width: parseInt(`${displayWorkAreaSize.width * 0.85}`, 10),
+      height: parseInt(`${displayWorkAreaSize.height * 0.85}`, 10),
+      center: true,
+      resizable: true,
+    });
+    const url = require('url').format({
+      protocol: 'file',
+      slashes: true,
+      pathname: require('path').join(__dirname, '../index.html')
+    })
+    mainWindow.loadURL(url);
+    mainWindow.on('ready-to-show', () => {
+      mainWindow.show();
+    });
+  }
+}
+app.whenReady().then(() => new createWin());
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
